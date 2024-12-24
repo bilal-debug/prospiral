849ae2843f3ae3f124424e45c1af1eaa6f155b76
bilal-debug <mrcoder815@gmail.com>
2024-12-24 14:22:47 +0500

    images without loop and easy

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
+// Unsplash Images API request
 
- // Unsplash Images API request
+const apiKey = "rba6OSYhYgsFIoI0ZLr-mpA1Vp8pqCYjbuDY9g9B3oA";
+const images = document.querySelectorAll(".image");
 
- const apiKey = 'rba6OSYhYgsFIoI0ZLr-mpA1Vp8pqCYjbuDY9g9B3oA';
- const images = document.querySelectorAll('.image');
- 
- 
- fetch(`https://api.unsplash.com/photos/random?query=restaurant&count=10&client_id=${apiKey}`)
-   
- .then(response => response.json())
-   
-   .then(data => {
-    
-    
-     data.forEach((photo, index) => {
-        (document.getElementById('author-image').src=photo.urls.regular);
-        (document.getElementById('card-image').src=photo.urls.regular);
+fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const banner_image = data.urls.regular;
+    const author_image = data.user.profile_image.small;
 
-      
-       
-     });
-   });
- 
-  
- 
-  async function fetchData() {
-    const response = await fetch('data.json');
-    const data = await response.json();
-  
-    document.getElementById('title').innerText = data.title;
-    document.getElementById('subtitle').innerText = data.subtitle;
-    document.getElementById('description').innerText = data.description;
-    document.getElementById('author-name').innerText = data.author.name;
-    
-  }
-  
+    document.getElementById("author-image").src = author_image;
+    document.getElementById("card-image").src = banner_image;
+  });
 
-  function init() {
-    
-    fetchData();
-  }
-  
-  window.onload = init;
-  
+async function fetchData() {
+  const response = await fetch("data.json");
+  const data = await response.json();
+
+  document.getElementById("title").innerText = data.title;
+  document.getElementById("subtitle").innerText = data.subtitle;
+  document.getElementById("description").innerText = data.description;
+  document.getElementById("author-name").innerText = data.author.name;
+}
+
+// function init() {
+  fetchData();
+// }
+
+// window.onload = init;
